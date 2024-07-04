ae8942553a6ce50d68ff23a30140832c64951e69
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-07-04 12:31:00 +0200

    make config

---
+++ boot.php
@@ -0,0 +1,21 @@
+<?php
+
+// ініціалізуємо сесію
+session_start();
+
+function pdo() : PDO
+{
+  static $pdo;
+
+  if(!$pdo) {
+    $config = include __DIR__.'/config.php';
+
+    // підключаємо бд
+    $dsn = 'mysql:dbname='.$config['db_name'].':host='.$config['db_host'];
+    $pdo = new PDO ($dsn, $config['db_user'], $config['db_pass']);
+    $pdo = setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+
+  return $pdo;
+}
+
---
+++ example.config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "db_name" => "name",
+    "db_host" => "host",
+    "db_user" => "user",
+    "db_pass" => "pass",
+];
